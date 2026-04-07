4c96f8e8115ab2b93e21ab1dced38820667a161c
Tyrell Wellick <keshavniure99@gmail.com>
2026-04-07 07:02:26 -0800

    feat: update BookCard to fetch and display 20 books per category

--- Frontend/src/Components/BookCard.jsx
+++ Frontend/src/Components/BookCard.jsx
@@ -6,19 +6,28 @@ function BookCard({category}) {
       useEffect(() =>{
         const FetchBooks = async () =>{
           try {
-            const response = await fetch(`https://openlibrary.org/subjects/${category.toLowerCase()}.json?limit=10`)
+            const response = await fetch(`https://openlibrary.org/subjects/${category.toLowerCase()}.json?limit=20`)
            const data = await response.json();
            setBooks(data.works);
+           console.log(data.works);
+           
           } catch (err) {
             console.error("Error fetching books:", err);
           }
         }
         FetchBooks();
-      }, []);
+      }, [category]);
   return (
     <div>
-      <h3>{books.length} books</h3>
+      <h2 className='font-bold'>{category}</h2>
+    <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
+      {books.map((book) => (
+        <div key={book.key}>
+          {book.title}
+        </div>
+      ))}
     </div>
+     </div>
   )
 }
 
